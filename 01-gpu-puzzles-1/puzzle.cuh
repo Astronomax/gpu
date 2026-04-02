48788b2baa8f269bf4d47b34d2a90c94099cf632
Astronomax <fgfgfb93@gmail.com>
2026-04-02 22:53:36 +0300

    task 2

--- 01-gpu-puzzles-1/puzzle.cuh
+++ 01-gpu-puzzles-1/puzzle.cuh
@@ -1,17 +1,22 @@
 #pragma once
 
 __global__ void Map(const float* data, float* out) {
-    // YOUR CODE HERE
+    out[threadIdx.x] = data[threadIdx.x] + 10;
 }
 
 __global__ void Zip(const float* left, const float* right, float* out) {
-    // YOUR CODE HERE
+    out[threadIdx.x] = left[threadIdx.x] + right[threadIdx.x];
 }
 
 __global__ void Guard(const float* data, float* out, size_t size) {
-    // YOUR CODE HERE
+    if (threadIdx.x < size) {
+        out[threadIdx.x] = data[threadIdx.x] + 10;
+    }
 }
 
 __global__ void Block(const float* data, float* out, float value, size_t size) {
-    // YOUR CODE HERE
+    size_t index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < size) {
+        out[index] = data[index] + value;
+    }
 }
