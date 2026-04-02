7a61e5d79af38fb76d067ef231f9a0a4fa2b25e5
Astronomax <fgfgfb93@gmail.com>
2026-04-02 23:04:31 +0300

    task 3

--- 01-device-add/add.cuh
+++ 01-device-add/add.cuh
@@ -1,26 +1,45 @@
 #pragma once
 
+#include <cstddef>
 #include <vector>
 
 #include <cuda_helpers.h>
 
-float* AllocDeviceVector(size_t num_elements) {
-    // YOUR CODE HERE
+__global__ void AddDeviceVectorsKernel(const float* left_device, const float* right_device,
+                                       float* out_device, size_t size) {
+    const size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size) {
+        out_device[idx] = left_device[idx] + right_device[idx];
+    }
 }
 
-void FreeDeviceVector(float* device_ptr) {
-    // YOUR CODE HERE
+float* AllocDeviceVector(size_t size) {
+    float* vector_device = nullptr;
+    CheckStatus(cudaMalloc(&vector_device, size * sizeof(float)));
+    return vector_device;
 }
 
-void CopyHostVectorToDevice(const std::vector<float>& vector_host, float* dst_device_ptr) {
-    // YOUR CODE HERE
+void FreeDeviceVector(float* vector_device) {
+    CheckStatus(cudaFree(vector_device));
 }
 
-std::vector<float> CopyDeviceVectorToHost(const float* ptr_device, size_t num_elements) {
-    // YOUR CODE HERE
+void CopyHostVectorToDevice(const std::vector<float>& src_host, float* dst_device) {
+    CheckStatus(cudaMemcpy(dst_device, src_host.data(), src_host.size() * sizeof(float),
+                           cudaMemcpyHostToDevice));
+}
+
+std::vector<float> CopyDeviceVectorToHost(const float* src_device, size_t size) {
+    std::vector<float> dst_host(size);
+    CheckStatus(
+        cudaMemcpy(dst_host.data(), src_device, size * sizeof(float), cudaMemcpyDeviceToHost));
+    return dst_host;
 }
 
 void AddDeviceVectors(const float* left_device, const float* right_device, float* out_device,
-                      size_t num_elements) {
-    // YOUR CODE HERE
+                      size_t size) {
+    constexpr size_t blockSize = 256;
+    const size_t blocks = (size + blockSize - 1) / blockSize;
+    AddDeviceVectorsKernel<<<blocks, blockSize>>>(left_device, right_device, out_device, size);
+    CheckStatus(cudaGetLastError());
+    CheckStatus(cudaDeviceSynchronize());
 }
